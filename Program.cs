549a42243926f4a10f94e4191be0a4f679c6d701
Mohamed Fathy <codetwostart@gmail.com>
2019-10-07 22:03:13 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pointertype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int i = 5;
+                int* p = &i;
+                Console.WriteLine((long)p);
+                Console.WriteLine(*p);
+            }
+
+        }
+    }
+}
